f2e62894c92d7bf57966017b2eb10e0efa72fb67
kushaljk11 <kattelkushal3@gmail.com>
2025-11-23 17:59:44 +0545

    initial commit

--- EasyKothaFrontend/src/App.jsx
+++ EasyKothaFrontend/src/App.jsx
@@ -1,10 +1,16 @@
 
 import "./App.css";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+      <Register />
+      <Login />
+    </BrowserRouter>
     </>
   );
 }
