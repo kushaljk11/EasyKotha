7bacfe5d6b29b1dcae5725a3bcbf42f17168a2f5
kushaljk11 <kattelkushal3@gmail.com>
2026-03-13 15:41:45 +0545

    updated cors

--- EasyKothaBackend/server.js
+++ EasyKothaBackend/server.js
@@ -21,7 +21,7 @@ dotenv.config();
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 }));
 
 app.use(express.json());
